9e09bddd11c0f407a5081d91e505c663064e3c37
naresh-sabapathy92 <reshs92.09@gmail.com>
2022-02-16 19:34:27 +0800

    #storybook

--- src/stories/CardDisplay.stories.js
+++ src/stories/CardDisplay.stories.js
@@ -1,5 +1,4 @@
 import { useState } from 'react';
-import { Typography } from 'antd';
 import { CardDisplay } from "../components";
 
 export default {
@@ -9,29 +8,24 @@ export default {
 
 export const Card = () => {
 
-    const { Title } = Typography;
-
     const [loading, setLoading] = useState(true);
 
     setTimeout(() => setLoading(false), 2000);
 
     return (
         <>
-            <Title level={4}>Display Card</Title>
             <CardDisplay title='Display Card' loading={loading} hoverable>
                 <p>Card content</p>
                 <p>Card content</p>
                 <p>Card content</p>
             </CardDisplay>
             <br />
-            <Title level={4}>Display Card Primary</Title>
             <CardDisplay classType='primary' style={{ width: '50%' }} hoverable >
                 <p>Card content</p>
                 <p>Card content</p>
                 <p>Card content</p>
             </CardDisplay>
             <br />
-            <Title level={4}>Display Card Header style alternative</Title>
             <CardDisplay title='Display Card' loading={loading} header='primary'>
                 <p>Card content</p>
                 <p>Card content</p>
